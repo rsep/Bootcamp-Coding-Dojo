db67c30102f57d78bd02a9e5195d94ef223bbae9
rsep <78937557+rsep@users.noreply.github.com>
2022-08-28 17:59:36 -0400

    Práctica Tarjeta Usuario HTML + JS

--- Web_Fundamentos/introHTML/practicas/tarjetaUsuario/js/script.js
+++ Web_Fundamentos/introHTML/practicas/tarjetaUsuario/js/script.js
@@ -2,6 +2,19 @@ console.log("Página Cargada");
 
 function clic_editar(){
     alert("Editando Perfil");
+
+    // seleccionar un elemento en especifico para cambiarlo
+    var elemento_p = document.querySelector('#descripcion'); // seleccionando elemento con id descripcion
+    console.log(elemento_p);
+    elemento_p.innerText = "Desarrolladora, empoderada, creativa y muy divertida";
+
+    // modificar imagend de perfil
+    var imagen_perfil = document.querySelector('.card-header img');
+    imagen_perfil.src = 'images/foto4.jpg';
+
+    // Agregar clase  a h1 (en mi caso es h2)
+    var nombre = document.querySelector('h2');
+    nombre.classList.add('purple');
 };
 
 
@@ -19,3 +32,28 @@ function over(){
 function out(){
     console.log("El mouse está fuera de Jane");
 };
+
+function cambieNombre(elemento_h2){
+    // recibimos el elemento h2
+    console.log(elemento_h2);
+
+    // hacer un sitch de nombres
+    if (elemento_h2.innerText === "Jane Doe"){
+        elemento_h2.innerText = "Juana De Arco";
+    } else{
+        elemento_h2.innerText = "Jane Doe";
+    }
+}
+
+function cambiaImagen(elemento_img){
+    console.log(elemento_img.src); //nos da la ruta completa
+    if (elemento_img.src.includes("images/map-marker.png")){ //para preguntar si la ruta incluye el nombre del argumento
+        elemento_img.src = "images/gear.png";
+    } else {
+        elemento_img.src = "images/map-marker.png";
+    }
+}
+
+function elimine(elemento_p){
+    elemento_p.remove(); //elimina el elemento al que hacemos doble clic
+}
